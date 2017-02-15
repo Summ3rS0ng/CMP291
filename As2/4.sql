19f78db5f8dfa491e6172f04fe1d6806f1dbc9cc
Evan Robertson <er2@cs.ualberta.ca>
2017-02-14 22:11:40 -0700

    updated to correct version

--- As2/4.sql
+++ As2/4.sql
@@ -1,12 +1,8 @@
 prompt Question4-er2
 
-select * from (select flwee, count(flwee) as countedf 
-from follows
-group by flwee) f Join
-(select Writer, count(term) as countedt from mentions 
+select * from (select Writer, count(term) as countedt
+from mentions 
 where term = 'edmonton'
 group by writer) m
-on f.flwee = m.Writer
-where countedf >0
-and countedt > 2;
+where countedt >= 2 and writer Not in(select flwee from follows) ;
 
