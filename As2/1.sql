b820fd361940a1927a8e85895dd459c1ba44bbf0
Summ3rS0ng <er2@ualberta.ca>
2017-03-02 19:33:37 -0700

    D C C deleted

--- As2/1.sql
+++
@@ -1,5 +0,0 @@
-prompt Question1-er2
-
-Select usr 
-from users
-Where Exists(Select * from tweets where users.usr = tweets.writer) AND Exists (Select * from follows where users.usr = followers.flwee);
